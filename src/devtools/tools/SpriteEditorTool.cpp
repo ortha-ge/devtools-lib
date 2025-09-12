a499ba11345634431e4aa1b69daee97b3144b1c7
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-12 13:27:17 -0400

    Started working on adding frames to sprite

--- src/devtools/tools/SpriteEditorTool.cpp
+++ src/devtools/tools/SpriteEditorTool.cpp
@@ -1,17 +1,86 @@
 module;
 
+#include <imgui.h>
+#include <imgui_stdlib.h>
 #include <entt/entt.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
 
 module DevTools.SpriteEditorTool;
 
+import Core.JsonTypeLoaderAdapter;
+import Core.ResourceLoadRequest;
+import Core.Spatial;
+import Core.TypeLoader;
 import DevTools.Tool;
+import Gfx.Camera;
+import Gfx.MaterialDescriptor;
+import Gfx.RenderObject;
+import Gfx.SpriteObject;
+import Gfx.Reflection.MaterialDescriptor;
 
 namespace DevTools {
 
 	void SpriteEditorTool::setup(entt::registry& registry) {
 		const entt::entity testTool{ registry.create() };
 		registry.emplace<Tool>(testTool, "Sprite Editor", [this](entt::registry& registry, Tool& tool) {
+			if (mSpriteObjectRoot == entt::null) {
+				mSpriteObjectRoot = registry.create();
+				registry.emplace<Core::Spatial>(mSpriteObjectRoot, glm::vec3{}, glm::vec3(5.0f, 5.0f, 1.0f));
+				registry.emplace<Gfx::RenderObject>(mSpriteObjectRoot);
+				registry.emplace<Gfx::SpriteObject>(mSpriteObjectRoot, mSprite);
+			}
 
+			if (ImGui::InputText("Material Resource File Path", &mMaterialResourceFilePath)) {
+				auto& renderObject{ registry.get<Gfx::RenderObject>(mSpriteObjectRoot) };
+				renderObject.materialResource = Core::ResourceLoadRequest::create<Core::TypeLoader>(
+					registry, mMaterialResourceFilePath,
+					std::make_shared<Core::JsonTypeLoaderAdapter<Gfx::MaterialDescriptor>>());
+			}
+
+			bool changed = false;
+			if (ImGui::CollapsingHeader("Frames")) {
+				for (size_t i = 0; i < mSprite.frames.size(); ++i) {
+					if (ImGui::TreeNode(std::format("Frame {}", i).c_str())) {
+						auto& frame{ mSprite.frames[i] };
+						changed =
+							ImGui::InputFloat2("Bottom Left", &frame.bottomLeft[0]) ||
+							ImGui::InputFloat2("Top Right", &frame.topRight[0]);
+						ImGui::TreePop();
+					}
+				}
+			}
+
+			if (ImGui::Button("Add Frame")) {
+				mSprite.frames.emplace_back(glm::vec2{ 0.0f, 0.0f }, glm::vec2{ 100.0f, 100.0f });
+				changed = true;
+			}
+
+			if (changed) {
+				registry.get<Gfx::SpriteObject>(mSpriteObjectRoot).spriteResource = mSprite;
+			}
+
+			// Sync with the camera position
+			auto cameraView = registry.view<Gfx::Camera>();
+			if (cameraView.empty()) {
+				return;
+			}
+
+			const auto& cameraEntity = cameraView.front();
+			if (!registry.all_of<Core::Spatial>(cameraEntity)) {
+				return;
+			}
+
+			const auto& cameraSpatial = registry.get<Core::Spatial>(cameraEntity);
+
+			auto& renderObjectSpatial{ registry.get<Core::Spatial>(mSpriteObjectRoot) };
+			renderObjectSpatial.position = cameraSpatial.position + mSpriteRenderPos;
+
+		}, [this](entt::registry& registry, Tool& tool) {
+			if (mSpriteObjectRoot != entt::null) {
+				registry.destroy(mSpriteObjectRoot);
+				mSpriteObjectRoot = entt::null;
+			}
 		});
 	}
 
--- src/devtools/tools/SpriteEditorTool.cppm
+++ src/devtools/tools/SpriteEditorTool.cppm
@@ -1,8 +1,15 @@
 module;
 
+#include <string>
+
 #include <entt/fwd.hpp>
+#include <entt/entity/entity.hpp>
+#include <glm/vec3.hpp>
 
 export module DevTools.SpriteEditorTool;
+
+import Core.ResourceHandle;
+import Gfx.Sprite;
 
 export namespace DevTools {
 
@@ -10,6 +17,15 @@ export namespace DevTools {
 	public:
 
 		void setup(entt::registry&);
+
+	private:
+
+		std::string mMaterialResourceFilePath;
+		Gfx::Sprite mSprite;
+
+		entt::entity mSpriteObjectRoot{ entt::null };
+		glm::vec3 mSpriteRenderPos{ 225.0f, 150.0f, 10.0f };
+		uint32_t mSelectedFrame{ 0u };
 
 	};
 
