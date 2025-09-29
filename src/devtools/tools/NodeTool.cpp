78f40e4492b092434bc82374da4b9944d87ca2d9
Niamh Cuileann <sweet.niamhy@gmail.com>
2025-09-29 14:00:06 -0400

    Right click node context menu, with added 'export JSON' button

--- src/devtools/tools/NodeTool.cpp
+++ src/devtools/tools/NodeTool.cpp
@@ -1,5 +1,6 @@
 module;
 
+#include <format>
 #include <memory>
 #include <vector>
 
@@ -7,16 +8,25 @@ module;
 
 module DevTools.NodeTool;
 
+import Core.Any;
 import Core.EnTTNode;
+import Core.JsonTypeSaverAdapter;
+import Core.Log;
 import Core.Node;
 import Core.NodeHandle;
 import Core.TypeId;
+import Core.TypeSaver;
+import Core.Reflection.Node;
 import DevTools.SelectedEntity;
 import DevTools.SelectedSceneRoot;
 import DevTools.Tool;
 import entt;
 
 namespace DevTools::NodeToolInternal {
+
+	constexpr const char* PrintExportFormatString = R"(Node JSON
+=============
+{})";
 
 	void selectNodeEntity(entt::registry& registry, Core::Node::Ptr node) {
 		using namespace Core;
@@ -54,22 +64,43 @@ namespace DevTools::NodeToolInternal {
 		registry.emplace<SelectedSceneRoot>(nodeEntity);
 	}
 
-	void recurseNodeTree(entt::registry& registry, Core::Node::Ptr node) {
-		if (ImGui::TreeNode(node->getName().c_str())) {
+	void exportNodeJSON(entt::registry& registry, Core::Node::Ptr node) {
+		std::string exportedJSON{ Core::save(registry, Core::Any{ *node.get() }) };
+		Core::logEntry(registry, PrintExportFormatString, exportedJSON);
+	}
+
+	void recurseNodeTree(entt::registry& registry, Core::Node::Ptr node, size_t idIndex = 0u) {
+		ImGui::PushID(std::format("{}_{}", node->getName(), idIndex).c_str());
+		const bool isTreeOpen = ImGui::TreeNode(node->getName().c_str());
+
+		if (ImGui::BeginPopupContextItem()) {
 			if (ImGui::Button("Select")) {
 				selectNodeEntity(registry, node);
+				ImGui::CloseCurrentPopup();
 			}
 
 			if (ImGui::Button("Set As Scene Root")) {
 				selectSceneRootEntity(registry, node);
+				ImGui::CloseCurrentPopup();
 			}
 
-			for (auto&& childNode : node->getChildren()) {
-				recurseNodeTree(registry, childNode);
+			if (ImGui::Button("Export Node JSON")) {
+				exportNodeJSON(registry, node);
+				ImGui::CloseCurrentPopup();
+			}
+
+			ImGui::EndPopup();
+		}
+
+		if (isTreeOpen) {
+			const auto& children{ node->getChildren() };
+			for (size_t i = 0; i < children.size(); ++i) {
+				recurseNodeTree(registry, children[i], i);
 			}
 
 			ImGui::TreePop();
 		}
+		ImGui::PopID();
 	}
 
 } // namespace DevTools::NodeToolInternal
